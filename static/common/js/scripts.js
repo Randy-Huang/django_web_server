d2b14805d313e8d2b429fb53d6eeffd8cbf8a81e
Randy-Huang <randy.hunang@gmail.com>
2018-02-06 23:12:38 +0800

    Update block UI test

--- static/common/js/scripts.js
+++ static/common/js/scripts.js
@@ -118,14 +118,18 @@ $(window).load(function(){
     $("#hornav").sticky({ topSpacing: 0 });
 });
 
-$(function () {
-    $("#btn").click(function () {
-        $.blockUI({
-            message: '<h1>讀取中...</h1>'
-        });
+$(document).ready(function() {
+    $('#demo').click(function() {
+        $.blockUI({ css: {
+            border: 'none',
+            padding: '15px',
+            backgroundColor: '#000',
+            '-webkit-border-radius': '10px',
+            '-moz-border-radius': '10px',
+            opacity: .5,
+            color: '#fff'
+        } });
 
-        setTimeout(function () {
-            $.unblockUI();
-        }, 3000);
+        setTimeout($.unblockUI, 2000);
     });
 });
